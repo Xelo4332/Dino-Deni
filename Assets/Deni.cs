88750a68a9ecebfd3f003d468cd66b64135cf213
Sliemon-Floof <89981972+Sliemon-Floof@users.noreply.github.com>
2022-05-17 12:51:28 +0200

    asdghjkl

--- Assets/Deni.cs
+++ Assets/Deni.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Deni : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private bool isGrounded;
     [SerializeField]
     private KeyCode jump;
     // Start is called before the first frame update
@@ -18,14 +19,21 @@ public class Deni : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
-    private void OnCollisionStay2D(Collision2D collision)
-    {
-        if (Input.GetKeyDown(jump))
+        if (Input.GetKeyDown(jump) && isGrounded)
         {
             rb.AddForce(Vector2.up * jumpMulti);
             print("jump");
         }
+    }
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGrounded = true;
+        print("on ground");
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isGrounded = false;
+        print("off ground");
     }
 }
