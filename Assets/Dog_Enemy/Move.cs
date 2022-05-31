d9710f3993c799ee67755f2f60337d22bebd68c2
Sliemon-Floof <89981972+Sliemon-Floof@users.noreply.github.com>
2022-05-31 14:38:13 +0200

    Update Move.cs

--- Assets/Dog_Enemy/Move.cs
+++ Assets/Dog_Enemy/Move.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
+    //Lets you chose the direction to move the dog in
     [SerializeField]
     private Vector3 move;
-    // Start is called before the first frame update
+
     void Start()
     {
+        //Moves the dog on the first frame
         transform.position += move;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Moves the dog every frame
         transform.position += move*Time.deltaTime;
     }
 }
